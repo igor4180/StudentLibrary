5c017ab1af457a7c843aa111a62607d66b5a5a32
student <student@DESKTOP-DEALCLD>
2022-10-21 20:49:48 +0300

    Добавьте файлы проекта.

---
+++ StudentLibrary/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace StudentLibrary
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public  DateTime DateOfBirth { get; set; }
+        public override string ToString()
+        {
+            return $"Surname: { LastName }, Name: { FirstName }, DateOfBirth: { DateOfBirth.ToLongDateString() }";
+        }
+    }
+}
---
+++ StudentLibrary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using static System.Console;
+namespace SimpleDLLTest
+{
+    public class DllImportExample
+    {
+        [DllImport("User32.dll")]
+        public static extern int
+        SetForegroundWindow(IntPtr point);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? processName = "notepad.exe", text;
+            Write("Enter text: ");
+            text = ReadLine();
+            Process p = Process.Start(processName);
+            p.WaitForInputIdle();
+            IntPtr h = p.MainWindowHandle;
+            DllImportExample.SetForegroundWindow(h);
+            SendKeys.SendWait(text);
+        }
+    }
+}
